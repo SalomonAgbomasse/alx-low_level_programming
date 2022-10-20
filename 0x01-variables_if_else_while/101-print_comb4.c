9ad1edaf90883872b60854ea9c5cdb93cd780e33
SalomonAgbomasse <salomonagbomasse25@gmail.com>
2022-10-20 16:57:17 -0700

    new

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,15 +18,12 @@ int main(void)
 
 
 	for (i = 48; i < 56; i++)
-
 	{
 
 		for (j = i + 1; j < 57; j++)
-
 		{
 
 			for (k = j + 1; k < 58; k++)
-
 			{
 
 				putchar(i);
@@ -36,7 +33,6 @@ int main(void)
 				putchar(k);
 
 				if (i != 55 || j != 56 || k != 57)
-
 				{
 
 					putchar(',');
