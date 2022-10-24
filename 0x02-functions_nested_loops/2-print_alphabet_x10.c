8356cf8330072b9eb62a20ebf3a0a9bee7b5f1dc
SalomonAgbomasse <salomonagbomasse25@gmail.com>
2022-10-24 07:04:51 -0700

    New commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,39 +1,22 @@
 #include  "main.h"
 
-
 /**
- * main - Prints the alphabet in lowercase.
- * Return: Always 0.
+ * main -print_alphabet_x10
+ * print_alphabet_x10p Prints the alphabet in lowercase.
  */
 
 void print_alphabet_x10(void)
-
 {
-
 	char c;
+	int i = 0;
 
-	int i=0;
-
-	
-
-	while (i++<=9)
-
+	while (i++ <= 9)
 	{
-
-		for(c = 'a'; c <= 'z'; c++)
-
+		for (c = 'a'; c <= 'z'; c++)
 		{
-
 			_putchar(c);
-
-			_putchar('\n');
+			_putchar('/n');
 
 		}
-
 	}
-
-
-
-	
-
 }
